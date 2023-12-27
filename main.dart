bfce039f1b859c79077e21110439da9c1e6b439c
engektiar <eng.ektiar@gmail.com>
2023-12-27 23:02:05 +0600

    operation part

--- main.dart
+++ main.dart
@@ -1,24 +1,48 @@
 void main() {
-  int a = 20;
-  int b = 30;
-  int c = a + b;
-  print("Result is= $c");
+  // int a = 20;
+  // int b = 30;
+  // int c = a + b;
+  // print("Result is= $c");
 
-  // list data type
-  List data = ['one', 'two', 'three'];
-  print(data);
-  Map<String, dynamic> mydatabase = {
-    "name": "ektiar hossen",
-    "age": 30,
-  };
-  print(mydatabase);
+  // // list data type
+  // List data = ['one', 'two', 'three'];
+  // print(data);
+  // Map<String, dynamic> mydatabase = {
+  //   "name": "ektiar hossen",
+  //   "age": 30,
+  // };
+  // print(mydatabase);
 
-  Set myset = {'one', 'two', 'three'};
-  print(myset);
+  // Set myset = {'one', 'two', 'three'};
+  // print(myset);
 
-  final nameto = 'ektiar';
-  print(nameto.codeUnits);
+  // final nameto = 'ektiar';
+  // print(nameto.codeUnits);
 
-  Runes input = Runes('\u{1f49b}');
-  print(String.fromCharCodes(input));
+  // Runes input = Runes('\u{1f49b}');
+  // print(String.fromCharCodes(input));
+
+// dart operator start
+  int a = 10;
+  int b = 3;
+  var c = a ~/ b;
+  //  ~ai singe ta use korala 3.00 paine por data show kora na
+  print(c);
+
+// type test operators
+  String name = "ektiar";
+  var result = name is int;
+  print(result);
+
+  // conditional operator
+  // tarnary operator example
+  String color = "red";
+  var resutl = color == 'red' ? 'color is red' : 'unknown';
+  print(resutl);
+  // null check operator
+  int? age;
+  var results = age ?? 20;
+  print(results);
+
+// dart operator end
 }
