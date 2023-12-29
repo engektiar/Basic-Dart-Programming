a4c52a31748e0f2d01a3866050ba9cfe43d82133
engektiar <eng.ektiar@gmail.com>
2023-12-29 23:15:06 +0600

    inheratinec add kora hoisa

--- oop/index.dart
+++ oop/index.dart
@@ -4,4 +4,19 @@ void main() {
   var obj = Example();
   obj.myFunction();
   obj.myfunctionTwo();
+
+  var sonobj = son();
+  sonobj.myfather();
+  print(sonobj.name);
+}
+
+class father {
+  int age = 50;
+  myfather() {
+    print("My father age is = $age");
+  }
+}
+
+class son extends father {
+  String name = "this is ektiar Hossen";
 }
